28aee516e90f48c8bfc4dc1b0a507191a02a9155
Matej Risek <matej.risek@hashicorp.com>
2023-02-14 14:05:12 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -9,7 +9,7 @@ class MyStack extends TerraformStack {
   constructor(scope: Construct, id: string) {
     super(scope, id);
     
-    console.log("ok");
+    console.log("okII");
 
     // define resources here
     new RandomProvider(this, "random");
